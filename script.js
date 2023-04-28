f8b3d83a2637f28f62df7d3502e0a05cb53ec0ab
Oleg Ershov <oecorango@gmail.com>
2023-04-28 11:46:21 +0400

    feat: add arrays with keyCode, en-key, rus-key, initialize adding keys

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-const BUTTONS = [
-  192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
-  9, 81, 87, 69, 82, 84, 89, 85, 79, 80, 219, 221, 220,
-  20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13,
-  16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 38,
-  17, 91, 18, 32, 18, 93, 17, 37, 40, 39,
+let KEY_CODE = [
+  ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+  ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+  ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+  ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+  ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
 ];
 
-function initKey() {
+const BUTTONS_RUS = [
+  'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace',
+  'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
+  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+  'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift',
+  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl',
+];
+const BUTTONS_EN = [
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift',
+  'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►', 'Ctrl',
+];
 
+const BODY = document.querySelector('body');
+const textarea = document.createElement('textarea');
+const keyboard = document.createElement('div');
+
+textarea.className = 'input-text';
+keyboard.className = 'keyboard';
+BODY.append(textarea);
+BODY.append(keyboard);
+
+function initKey() {
+  let out = '';
+  for (let i = 0; i < BUTTONS_RUS.length; i++) {
+    // if (i === 14 || i === 29 || i === 42 || i === 55) {
+    //   out = '<div class="clearfix">' + '</div>';
+    // }
+    const keyboardKey = document.createElement('div');
+    keyboardKey.className = 'keyboard-key';
+    keyboard.append(keyboardKey);
+    out = `<div class="key-rus">${BUTTONS_RUS[i]}</div>`;
+    // <div class="key-en hidden">${BUTTONS_EN[i]}</div>;
+    keyboardKey.innerHTML = out;
+  }
 }
 
 initKey();
